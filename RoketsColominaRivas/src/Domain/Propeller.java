492d2b35b24e7aa905b5a3098dff8537b6612146
PolRivasBalaguero <59823842+PolRivasBalaguero@users.noreply.github.com>
2020-06-11 15:53:12 +0200

    V7

--- RoketsColominaRivas/src/Domain/Propeller.java
+++ RoketsColominaRivas/src/Domain/Propeller.java
@@ -10,28 +10,10 @@ public Propeller(double maxAcceleration) throws Exception {
     acceleration=0;
     this.maxAcceleration=maxAcceleration;
 }
-public double increaseAcceleration(double increase) {
-    if(acceleration+increase<=maxAcceleration) {
-        acceleration+=increase;
-        return 0;
-        }
-    else {
-    	double auxiliar= this.maxAcceleration - this.acceleration;
-    	this.acceleration=this.maxAcceleration;
-    	return increase-auxiliar;
-    }
-    
-}
-public double decreaseAcceleration(double decrease) {
-	if(acceleration-decrease>=0) {
-        acceleration-=decrease;
-        return 0;
-        }
-    else {
-    	double auxiliar=decrease-this.acceleration;
-    	this.acceleration=0;
-    	return auxiliar;
-    }    
+public void setAcceleration(double acceleration) throws Exception {
+	if(acceleration<1)
+		throw new Exception();
+	this.acceleration=acceleration;
 }
 public double getAcceleration() {
     return acceleration;
